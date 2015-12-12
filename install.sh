b6fb80e67f4f04589c9b65033178549e3f705c34
Zeb McCorkle <19mccorkle.zebulon@brenhamk-12.net>
2015-12-12 12:34:05 -0600

    Fix a typo

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ mkdir ~/building
 cd ~/building
 
 wget http://llvm.org/releases/3.7.0/llvm-3.7.0.src.tar.xz -O- | tar -xvf -
-wget http://llvm.org/released/3.7.0/cfe-3.7.0.src.tar.xz -O- | tar -xvf -
+wget http://llvm.org/releases/3.7.0/cfe-3.7.0.src.tar.xz -O- | tar -xvf -
 
 mkdir llvm
 cp -R llvm-3.7.0.src/* llvm/
