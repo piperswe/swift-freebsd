43c88b3f518b060b3d04dcf023f7977a863a4f4b
Zeb McCorkle <zmccorkle@classcoder.com>
2015-12-12 11:51:53 -0600

    Actually add the script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+mkdir ~/building
+cd ~/building
+
+wget http://llvm.org/releases/3.7.0/llvm-3.7.0.src.tar.xz -O- | tar -xvf -
+wget http://llvm.org/released/3.7.0/cfe-3.7.0.src.tar.xz -O- | tar -xvf -
+
+mkdir llvm
+cp -R llvm-3.7.0.src/* llvm/
+
+mkdir clang
+cp -R cfe-3.7.0.src/* clang/
+
+rm -rf llvm-3.7.0.src cfe-3.7.0.src
+
+git clone https://github.com/apple/swift
+
+cd swift
+utils/build-script
+
+echo "Done!"
